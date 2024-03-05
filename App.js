148af42f1f0c70950f9e7eef2991268479fb3820
guarani2614 <maria.barros111@etec.sp.gov.br>
2024-03-05 11:43:56 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Maria Beatriz Guarani de Barros</Text>
+      <Text>05/03/2024 DDM 3B3 Lado B</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
