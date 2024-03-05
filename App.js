91843b422462d110f62e6afaf329507e42e7fbf5
unknown <maria.barros111@etec.sp.gov.br>
2024-03-05 12:20:26 -0300

    clone

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ export default function App() {
     <View style={styles.container}>
       <Text>Maria Beatriz Guarani de Barros</Text>
       <Text>05/03/2024 DDM 3B3 Lado B</Text>
+      <Text>Novo Comit Clone</Text>
+      <Text>Novo Comit Clone</Text>
       <StatusBar style="auto" />
     </View>
   );
